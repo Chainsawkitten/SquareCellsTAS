5a77897e0616b6d2aa0010fc7e9824f1f24630a1
Chainsawkitten <catscratchesp@gmail.com>
2018-07-05 19:58:13 +0200

    Wait for R press

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <thread>
+#include <windows.h>
 
 using namespace std;
 
 int main(int argc, const char* argv[]) {
-    cout << "Hello world!" << endl;
+    cout << "Waiting for an R press..." << endl;
+
+    // Wait until the user presses R to begin.
+    while (!GetKeyState('R')) {
+        this_thread::yield();
+    }
+
+    cout << "Done." << endl;
 
     return 0;
 }
