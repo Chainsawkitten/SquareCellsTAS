ad08ec0fa71ffa3fc9d7fd4e8f594eb0cc891cd1
Chainsawkitten <catscratchesp@gmail.com>
2018-07-05 21:50:22 +0200

    Play level

---
+++ src/Timing.cpp
@@ -0,0 +1,12 @@
+#include "Timing.hpp"
+
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+namespace Timing {
+    void Wait(int frames) {
+        this_thread::sleep_for(chrono::microseconds(16667 * frames));
+    }
+}
---
+++ src/Timing.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Timing {
+    /**
+     * Wait for a certain number of frames.
+     * @param frames The number of frames to wait.
+     */
+    void Wait(int frames);
+}
