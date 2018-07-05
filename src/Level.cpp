8030d30a371710a0ebc8c00c65df4e1960e5b28e
Chainsawkitten <catscratchesp@gmail.com>
2018-07-05 20:27:37 +0200

    Check if level file exists

--- src/Level.cpp
+++ src/Level.cpp
@@ -1,5 +1,21 @@
 #include "Level.hpp"
 
+#include "FileSystem.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 Level::Level(int number) {
+    string filename = "data/" + to_string(number) + ".lvl";
+
+    if (!FileSystem::FileExists(filename.c_str())) {
+        cout << "Couldn't find " << filename << endl;
+        return;
+    }
+
+    // TODO: Load info from the file.
+
     loaded = true;
 }
