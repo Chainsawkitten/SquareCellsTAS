38bf9a243835a6cd9e81704d9e79a4b1ac3a2101
Chainsawkitten <catscratchesp@gmail.com>
2018-07-05 20:50:46 +0200

    Configuration file
    File stores configuration about SquareCells.

---
+++ src/configuration.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/// Width of the screen in pixels.
+const int SCREEN_WIDTH = 1920;
+
+/// Height of the screen in pixels.
+const int SCREEN_HEIGHT = 1080;
+
+/// Width of a cell in pixels.
+const int CELL_WIDTH = 47;
+
+/// Height of a cell in pixels.
+const int CELL_HEIGHT = 47;
