7bde55203c5cfced7e000b7220e2d68ffdf078c9
Chainsawkitten <catscratchesp@gmail.com>
2018-07-06 12:58:42 +0200

    Reduce level switch time
    New run: 4:05.571

--- src/Timing.cpp
+++ src/Timing.cpp
@@ -11,7 +11,7 @@ namespace Timing {
     }
 
     void WaitForLevelSwitch() {
-        this_thread::sleep_for(chrono::milliseconds(1000));
+        this_thread::sleep_for(chrono::milliseconds(900));
     }
 
     void WaitForLevelSelect() {
